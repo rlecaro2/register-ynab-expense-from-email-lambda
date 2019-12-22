b8ab5a08173002cd8d4ba819a2731d6289132401
Rafael Lecaros <rafael.lecaros@gmail.com>
2019-12-21 21:02:49 -0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   entry: ['./src/index.js'],
   target: 'node',
-  rules: [
-    {
-      test: /\.tsx?$/,
-      loader: 'babel-loader',
-    },
-  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
   output: {
     path: `${process.cwd()}/dist`,
     filename: 'index.js',
